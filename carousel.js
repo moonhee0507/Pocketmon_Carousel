09bda7b01cd3ba81e1170514a7c384140366f52c
Moon Hee <sjmh0507@naver.com>
2022-05-15 19:17:25 +0900

    2022-05-15 수정

--- carousel.js
+++ carousel.js
@@ -1,16 +1,11 @@
 let htmlStr = ''; 
 htmlStr += ` 
-  <ol>
-    <li>테마</li>
-    <li>이름</li>
-    <li>평점</li>
-    <li>썸네일</li>
-    <li>제목(title)</li>
-    <li>주인공이름</li>
-  </ol>
+  <div class><img src="./images/발챙이.png"></div>
+  <div><img src="./images/슈륙챙이.png"></div>
+  <div><img src="./images/강챙이.png"></div>
     
-    <br /> 
-    .... 
+  <br /> 
+  .... 
 `; 
     
 var list = document.getElementById("list"); 
